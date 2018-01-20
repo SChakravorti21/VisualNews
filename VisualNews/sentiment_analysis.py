34054ac9ad0047e226e618106638850f822781ef
Pranay Neelagiri <pneeldam@gmail.com>
2018-01-20 14:05:46 -0500

    added weighted sentiment analysis reddit

--- VisualNews/sentiment_analysis.py
+++ VisualNews/sentiment_analysis.py
@@ -2,8 +2,8 @@ import twitter, praw
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-api = twitter.Api(consumer_key="Ld3CmGVdyVLPL6gbzZHv5lxhN",
-                    consumer_secret="T4q1RbVjosngsi3WMJxuqCQC66HKvlRdh4J2G5qiH1jMtJsEIq",
+api = twitter.Api(consumer_key="O5oPqSVZjupmdq8l0cbpJc5Zh",
+                    consumer_secret="rg5Q4LziheNVjqdVvMY8RBsasZXVuS3CShOwp408NH0Tf7CREd",
                     access_token_key="954621614922125312-yAn0jF9zTLmflhoADnrOj01wA9VhM1o",
                     access_token_secret="HY4XvWJ6uKKEOMeuWYohemVSIqPpKAuJcz83HsBv2huVg")
 
@@ -34,18 +34,15 @@ def analyze_twitter_sentiment(kwds):
     tweets =[]
 
     kwd_list = ""
-    base_query = "l=en&q="
+
     for kwd in kwds:
         kwd_list = kwd_list + "{} ".format(kwd)
-        query = base_query + "{}&count=100".format(kwd)
-        print(query)
+    
+    queries = make_twitter_queries(kwds)
+    for query in queries:
         results = api.GetSearch(raw_query=query)
         for result in results:
             tweets.append(result.text)
-
-    count = 0
-    total = 0
-    multiplier = 1.0
 
     for tweet in tweets:
         doc_array = [tweet, kwd_list]
@@ -67,16 +64,52 @@ def analyze_twitter_sentiment(kwds):
     print("{} / {} = {}".format(total, count, float(total / count)))
     return float(total / count)
 
-def analyze_reddit_sentiment(kwds):
-    for submission in news.hot(limit=1):
-        print(submission.title)
-        comments = submission.comments
-        i = 0
-        for comment in comments:
-            if i > 5:
-                break
-            print(comment.body)
-            i += 1
-        
+def make_twitter_queries(kwds):
+    base_query = "l=en&q="
+    queries = []
 
-analyze_reddit_sentiment([])
+    i = 0
+    while i < 6:
+        query = base_query + "{}%20OR%20{}?count=100".format(kwds[i], kwds[i+1])
+        queries.append(query)
+        i += 2
+    
+    return queries
+
+def analyze_reddit_sentiment(kwds):
+    kwd_list = ""
+
+    count = 0.0
+    total = 0.0
+    multiplier = 1.0
+
+    for kwd in kwds:
+        kwd_list = kwd_list + "{} ".format(kwd)
+
+    for submission in news.hot(limit=25):
+        
+        doc_array = [submission.title, kwd_list]
+        similarity = get_text_similarity(doc_array=doc_array)
+        if similarity < .15:
+            continue
+        else:
+            print(submission.title)
+            multiplier = similarity / 0.1
+            comments = submission.comments
+            i = 0
+            while i < 25 and i < len(comments):
+                i += 1
+                vs = analyzer.polarity_scores(comments[i].body)
+                if vs['compound'] == 0.0:
+                    continue
+                total += vs['compound'] * multiplier
+                count += 1
+    
+    if total == 0.0:
+        print("none")
+        return None
+    else:
+        print(float(total / count))
+        return float(total / count)   
+
+#analyze_twitter_sentiment(["government", "shutdown", "Trump", "senate", "spending", "budget"])
